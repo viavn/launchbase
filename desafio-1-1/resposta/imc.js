f2f8af8478ae3eb3d186fd13e943472ac4e2fecf
Vinicius Avansini <vi.avn@hotmail.com>
2020-01-28 21:28:42 -0300

    Desafio Primeiros passos com JS

---
+++ desafio-1-1/resposta/imc.js
@@ -0,0 +1,14 @@
+const nome = 'Carlos';
+const peso = 84;
+const altura = 1.88;
+const imc = peso / (altura * altura);
+
+let message;
+
+if (imc >= 30) {
+    message = `${nome} você está acima do peso`;
+} else {
+    message = `${nome} você não está acima do peso`;
+}
+
+console.log(message);
