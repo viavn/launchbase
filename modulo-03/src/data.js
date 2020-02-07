1ae4b1e0df5a341d0fc9d74ba21aec4680858716
Vinicius Avansini <vi.avn@hotmail.com>
2020-02-07 20:01:14 -0300

    Desafios 01 e 02 - módulo 3

---
+++ modulo-03/src/data.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    id: 'HN1UjzRSdBk',
+    title: 'Desvendando o CSS Grid na prática',
+    duration: '36 min',
+    price: 'Free'
+  },
+  {
+    id: 'L0zuZYh800Q',
+    title:
+      'Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias',
+    duration: '38 min',
+    price: 'Free'
+  },
+  {
+    id: 'ngVU74daJ8Y',
+    title:
+      'Code/Drops #16 - Tema light/dark com React, Styled Components e TypeScript',
+    duration: '34 min',
+    price: 'Free'
+  },
+  {
+    id: '3tB0uDliS6Y',
+    title: 'Masterclass #05 - Masterizando o Insomnia',
+    duration: '35 min',
+    price: 'Free'
+  },
+  {
+    id: 'QfcozcbDhNM',
+    title: 'Podcast FalaDev #11 - Técnicas de estudo e aprendizado tradicional',
+    duration: '36 min',
+    price: 'Free'
+  },
+  {
+    id: 'py6vHgL6saw',
+    title: 'Podcast FalaDev #10 - Testes no back-end, TDD e estratégias',
+    duration: '42 min',
+    price: 'Free'
+  }
+]
--- modulo-03/src/server.js
+++ modulo-03/src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 const port = 5000
 
@@ -10,15 +11,29 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get('/', (req, res) => {
-  return res.render('about')
+  const about = {
+    avatar_url:
+      'https://media-exp1.licdn.com/dms/image/C4D03AQFsdwwmKay9kA/profile-displayphoto-shrink_200_200/0?e=1585785600&v=beta&t=qyPwJ1ONwxo1tEE5g4WfuhL_r1pOmZ69K0fBKK5my3I',
+    name: 'Vinícius Avansini',
+    role: 'Programador Júnior - Writesys',
+    description:
+      'Programador full-stack. Colaborador da <a href="https://writesys.com.br/" target="_blank">Writesys</a>',
+    links: [
+      { name: 'GitHub', url: 'https://github.com/viavn' },
+      { name: 'LinkedIn', url: 'https://www.linkedin.com/in/viniciusavansini' }
+    ]
+  }
+
+  return res.render('about', { about })
 })
 
 server.get('/portfolio', (req, res) => {
-  return res.render('portfolio')
+  return res.render('portfolio', { items: videos })
 })
 
 server.listen(port, () => {
