89284448988b1a3895a1ed7e367e6e0a156a4cd4
Vinicius Avansini <vi.avn@hotmail.com>
2020-02-29 16:06:57 -0300

    Módulo 04 - Listagem de instrutores

--- modulo-04/src/instructors.js
+++ modulo-04/src/instructors.js
@@ -2,6 +2,10 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+exports.index = function (req, res) {
+  return res.render('instructors/index', { instructors: data.instructors });
+}
+
 // show
 exports.show = function (req, res) {
   const { id } = req.params
@@ -91,7 +95,8 @@ exports.put = function (req, res) {
   const instructor = {
     ...foundInstructor,
     ...req.body,
-    birth: Date.parse(req.body.birth)
+    birth: Date.parse(req.body.birth),
+    id: Number(id)
   }
 
   data.instructors[index] = instructor
