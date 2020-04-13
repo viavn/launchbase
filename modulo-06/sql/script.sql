d13d1aebaa2df53ab99627b8e3ffa3c6068a22f5
Vinicius Avansini <vinicius.avansini@writesys.onmicrosoft.com>
2020-04-13 17:25:02 -0300

    Módul0 06 - Iniciando o projeto e modelagem de dados

---
+++ modulo-06/sql/script.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "Category" (
+  "Category_Id" SERIAL PRIMARY KEY,
+  "Category_Name" text not null
+);
+
+CREATE TABLE "Product" (
+  "Product_Id" SERIAL PRIMARY KEY,
+  "Product_Name" text not null,
+  "Product_Description" text not null,
+  "Product_OldPrice" int,
+  "Product_Price" int not null,
+  "Product_Quantity" int DEFAULT 0,
+  "Product_Status" int DEFAULT 1,
+  "Product_Created_At" timestamp DEFAULT (now()),
+  "Product_Updated_At" timestamp DEFAULT (now()),
+  "Category_Id" int UNIQUE,
+  "User_Id" int UNIQUE
+);
+
+CREATE TABLE "File" (
+  "File_Id" SERIAL PRIMARY KEY,
+  "File_Name" text not null,
+  "File_Path" text not null,
+  "Product_Id" int UNIQUE
+);
+
+ALTER TABLE "Product" ADD FOREIGN KEY ("Category_Id") REFERENCES "Category" ("Category_Id");
+
+ALTER TABLE "File" ADD FOREIGN KEY ("Product_Id") REFERENCES "Product" ("Product_Id");
